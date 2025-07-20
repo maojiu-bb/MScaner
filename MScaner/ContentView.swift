31c609f5a9b84f21bc5deb0f5a74da9a54239224
maojiu <maojiu-bb@outlook.com>
2025-07-20 16:37:58 +0800

    feat: init project

---
+++ MScaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MScaner
+//
+//  Created by 钟钰 on 2025/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MScaner/MScanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MScanerApp.swift
+//  MScaner
+//
+//  Created by 钟钰 on 2025/7/20.
+//
+
+import SwiftUI
+
+@main
+struct MScanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
